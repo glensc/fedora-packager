01137bb7d221de836a0caba93249685b235320af
Jesse Keating <jkeating@redhat.com>
2011-01-30 15:52:37 -0700

    Fix whitespace

--- src/pyfedpkg/__init__.py
+++ src/pyfedpkg/__init__.py
@@ -40,7 +40,7 @@ GITBASEURL = 'ssh://%(user)s@pkgs.fedoraproject.org/%(module)s'
 ANONGITURL = 'git://pkgs.fedoraproject.org/%(module)s'
 TRACBASEURL = 'https://%(user)s:%(password)s@fedorahosted.org/rel-eng/login/xmlrpc'
 UPLOADEXTS = ['tar', 'gz', 'bz2', 'lzma', 'xz', 'Z', 'zip', 'tff', 'bin',
-              'tbz', 'tbz2', 'tgz', 'tlz', 'txz', 'pdf', 'rpm', 'jar', 'war', 
+              'tbz', 'tbz2', 'tgz', 'tlz', 'txz', 'pdf', 'rpm', 'jar', 'war',
               'db', 'cpio', 'jisp', 'egg', 'gem']
 BRANCHFILTER = 'f\d\d\/master|master|el\d\/master|olpc\d\/master'
 
